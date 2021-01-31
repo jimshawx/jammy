fd2ae83d31b6ea8d37f73eaa4c139596c7a24adc
Jim <jimshaw@iinet.net.au>
2021-01-31 16:47:34 +0000

    more CIA improvements.

--- Custom/CIA.cs
+++ Custom/CIA.cs
@@ -1,8 +1,36 @@
-﻿using RunAmiga.Types;
+﻿using System;
+using System.Linq;
+using RunAmiga.Types;
 
 namespace RunAmiga.Custom
 {
 	//https://www.amigacoding.com/index.php?title=CIA_Memory_Map
+	//http://www.theflatnet.de/pub/cbm/amiga/AmigaDevDocs/hard_f.html#f-2-3
+	[Flags]
+	public enum CR
+	{
+		START=1,
+		PBON=2,
+		OUTMODE=4,
+		RUNMODE=8,
+		LOAD=16,
+		INMODE0=32,
+		CRB_INMODE1 = 64,
+		CRA_SPMODE = 64,
+		CRB_ALARM=128
+	}
+
+	[Flags]
+	public enum ICRB
+	{
+		TIMERA=1,
+		TIMERB=2,
+		TODALARM=4,
+		SERIAL=8,
+		FLAG=16,
+
+		IR=128,
+	}
 
 	public abstract class CIA : IEmulate, IMemoryMappedDevice
 	{
@@ -23,11 +51,18 @@ namespace RunAmiga.Custom
 		public const int CRA = 14;
 		public const int CRB = 15;
 
-		protected readonly byte[] regs = new byte[16];
+		protected readonly uint[] regs = new uint[16];
 
-		protected byte icrr;
-		//writing to regs[0xd] sets icr which controls which interrupts TO trigger.
+		private byte icrr;
+		//writing to regs[ICR] sets icr which controls which interrupts TO trigger.
 		//icrr has the equivalent bits set for which interrupt WAS triggered. reset to 0 after read.
+
+
+		private ushort timerA;
+		private ushort timerB;
+
+		private ushort timerAreset;
+		private ushort timerBreset;
 
 		protected Interrupt interrupt;
 
@@ -35,53 +70,52 @@ namespace RunAmiga.Custom
 
 		public virtual void Emulate(ulong cycles)
 		{
-
 			timerTime += cycles;
 			if (timerTime > 10)// timers tick at 1/10th cpu clock
 			{
 				timerTime -= 10;
 
 				//timer A running
-				if ((regs[CIA.CRA] & 1) != 0)
+				if ((regs[CIA.CRA] & (uint)CR.START) != 0)
 				{
 					//timer A
-					regs[CIA.TALO]--;
-					if (regs[CIA.TALO] == 0xff)
-						regs[CIA.TAHI]--;
+					timerA--;
 
-					if (regs[CIA.TALO] == 0 && regs[CIA.TAHI] == 0)
+					if (timerA == 0xffff)
 					{
-						if ((regs[CIA.ICR] & (1 << 0)) != 0)
+						if ((regs[CIA.ICR] & (uint)ICRB.TIMERA) != 0)
 						{
-							icrr |= (1 << 0) + 0x80;
+							icrr |= (byte)(ICRB.TIMERA | ICRB.IR);
 							interrupt.TriggerInterrupt(Interrupt.PORTS);
 						}
 
 						//one shot mode?
-						if ((regs[CIA.CRA] & (1 << 3)) != 0)
-							regs[CIA.CRA] &= 0xfe;
+						if ((regs[CIA.CRA] & (uint)CR.RUNMODE) != 0)
+							regs[CIA.CRA] &= ~(uint)CR.START;
+						else
+							timerA = timerAreset;
 					}
 				}
 
 				//timer B running
-				if ((regs[CIA.CRB] & 1) != 0)
+				if ((regs[CIA.CRB] & (uint)CR.START) != 0)
 				{
 					//timer B
-					regs[CIA.TBLO]--;
-					if (regs[CIA.TBLO] == 0xff)
-						regs[CIA.TBHI]--;
+					timerB--;
 
-					if (regs[CIA.TBLO] == 0 && regs[CIA.TBHI] == 0)
+					if (timerB == 0xffff)
 					{
-						if ((regs[CIA.ICR] & (1 << 1)) != 0)
+						if ((regs[CIA.ICR] & (uint)ICRB.TIMERB) != 0)
 						{
-							icrr |= (1 << 1) + 0x80;
+							icrr |= (byte)(ICRB.TIMERB | ICRB.IR);
 							interrupt.TriggerInterrupt(Interrupt.PORTS);
 						}
 
 						//one shot mode?
-						if ((regs[CIA.CRB] & (1 << 3)) != 0)
-							regs[CIA.CRB] &= 0xfe;
+						if ((regs[CIA.CRB] & (uint)CR.RUNMODE) != 0)
+							regs[CIA.CRB] &= ~(uint)CR.START;
+						else
+							timerB = timerBreset;
 					}
 				}
 			}
@@ -91,8 +125,13 @@ namespace RunAmiga.Custom
 		{
 			for (int i = 0; i < 16; i++)
 				regs[i] = 0;
-			regs[CIA.TAHI] = regs[CIA.TALO] = regs[CIA.TBHI] = regs[CIA.TBLO] = 0xff;
 
+			timerA = 0xffff;
+			timerB = 0xffff;
+			timerAreset = timerA;
+			timerBreset = timerB;
+
+			icrr = 0;
 		}
 
 		public virtual bool IsMapped(uint address)
@@ -112,39 +151,76 @@ namespace RunAmiga.Custom
 
 		protected uint Read(byte reg)
 		{
-			if (reg == CIA.ICR)
+			switch (reg)
 			{
-				byte p = icrr;
-				icrr = 0;
-				return p;
+				case CIA.ICR:
+				{
+					byte p = icrr;
+					icrr = 0;
+					return p;
+				}
+				case CIA.TAHI: return (uint)(timerA >> 8);
+				case CIA.TALO: return timerA;
+				case CIA.TBHI: return (uint)(timerB >> 8);
+				case CIA.TBLO: return timerB;
+				default: return (uint)regs[reg];
 			}
-
-			return (uint)regs[reg];
 		}
 
 		public abstract void Write(uint insaddr, uint address, uint value, Size size);
 
 		protected void Write(byte reg, uint value)
 		{
-			if (reg == CIA.ICR)
+			switch (reg)
 			{
-				if ((value & 0x80) != 0)
-					regs[CIA.ICR] |= (byte)value;
-				else
-					regs[CIA.ICR] &= (byte)~value;
-			}
-			else
-			{
-				regs[reg] = (byte)value;
-			}
+				case CIA.ICR:
+					if ((value & 0x80) != 0)
+						regs[CIA.ICR] |= (byte)value;
+					else
+						regs[CIA.ICR] &= (byte)~value;
+					break;
 
-			if (reg == CIA.TAHI)
-			{
-				regs[CIA.CRA] |= 1;
-			}
-			else if (reg == CIA.TBHI)
-			{
-				regs[CIA.CRB] |= 1;
+				case CIA.TAHI:
+					timerAreset = (ushort)((timerAreset & 0x00ffu) | (value << 8));
+					if ((regs[CIA.CRA] & (uint)CR.START) == 0)
+					{
+						timerA = timerAreset;
+						regs[CIA.CRA] |= (uint)CR.START; //start the timer
+					}
+					break;
+				case CIA.TALO:
+					timerAreset = (ushort)((timerAreset & 0xff00u) | value);
+					break;
+
+				case CIA.TBHI:
+					timerBreset = (ushort)((timerBreset & 0x00ffu) | (value << 8));
+					if ((regs[CIA.CRB] & (uint)CR.START) == 0)
+					{
+						timerB = timerBreset;
+						regs[CIA.CRB] |= (uint)CR.START;//start the timer
+					}
+					break;
+				case CIA.TBLO:
+					timerBreset = (ushort)((timerBreset & 0xff00u) | value);
+					break;
+
+				case CIA.CRA:
+					if ((value & (uint)CR.LOAD) != 0)
+						timerA = timerAreset;
+					value &= ~(uint)CR.LOAD;
+					regs[CIA.CRA] = (byte)value;
+					break;
+
+				case CIA.CRB:
+					if ((value & (uint)CR.LOAD) != 0)
+						timerB = timerBreset;
+					value &= ~(uint)CR.LOAD;
+					regs[CIA.CRB] = (byte)value;
+					break;
+
+				default:
+					regs[reg] = (byte)value;
+					break;
 			}
 		}
 	}
