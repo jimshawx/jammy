05573681a2741838f9146b51ad38006d08aa2dc3
Jim <jimshaw@iinet.net.au>
2024-01-05 14:58:11 +0200

    add support for single adf inside a zip file, and for adz/gzipped disks

--- Jammy.Core/Floppy/Disk.cs
+++ Jammy.Core/Floppy/Disk.cs
@@ -2,6 +2,7 @@
 using System.IO.Compression;
 using System.Linq;
 using System.Xml.Serialization;
+using static Jammy.Core.Floppy.RP9Manifest;
 
 /*
 	Copyright 2020-2021 James Shaw. All Rights Reserved.
@@ -19,6 +20,13 @@ namespace Jammy.Core.Floppy
 		{
 			if (!adfFileName.StartsWith(floppyPath))
 				adfFileName = Path.Combine(floppyPath, adfFileName);
+
+			if (!File.Exists(adfFileName))
+			{
+				data = null;
+				return;
+			}
+
 			data = File.ReadAllBytes(adfFileName);
 
 			//is it a zip file?
@@ -27,20 +35,37 @@ namespace Jammy.Core.Floppy
 				//try to unpack the first floppy from inside an rp9 zip archive
 				try
 				{
-					RP9Manifest rp9 = null;
 					using (var m = new MemoryStream(data))
 					{
 						using (var zip = new ZipArchive(m))
 						{
 							var manifest = zip.Entries.SingleOrDefault(x => x.Name == "rp9-manifest.xml");
-							var xml = new XmlSerializer(typeof(RP9Manifest));
-							using (var config = manifest.Open())
-								rp9 = (RP9Manifest)xml.Deserialize(config);
+							if (manifest != null)
+							{ 
+								//it's an RP9 file
+								RP9Manifest rp9 = null;
+								var xml = new XmlSerializer(typeof(RP9Manifest));
+								using (var config = manifest.Open())
+									rp9 = (RP9Manifest)xml.Deserialize(config);
 
-							var floppy0 = rp9.application.media.floppy.OrderBy(x => x.priority).FirstOrDefault();
-							if (floppy0 != null)
+								var floppy0 = rp9.application.media.floppy.OrderBy(x => x.priority).FirstOrDefault();
+								if (floppy0 != null)
+								{
+									var zippedFloppy = zip.Entries.FirstOrDefault(x => x.Name == floppy0.floppy);
+									using (var floppy = zippedFloppy.Open())
+									{
+										using (var ms = new MemoryStream())
+										{
+											floppy.CopyTo(ms);
+											data = ms.ToArray();
+										}
+									}
+								}
+							}
+							else
 							{
-								var zippedFloppy = zip.Entries.FirstOrDefault(x => x.Name == floppy0.floppy);
+								//it's just a zip with an adf file in it
+								var zippedFloppy = zip.Entries.SingleOrDefault(x=>x.Name.ToLower().EndsWith(".adf"));
 								using (var floppy = zippedFloppy.Open())
 								{
 									using (var ms = new MemoryStream())
@@ -49,6 +74,29 @@ namespace Jammy.Core.Floppy
 										data = ms.ToArray();
 									}
 								}
+							}
+						}
+					}
+				}
+				catch
+				{
+					//couldn't isolate a floppy file
+					data = null;
+				}
+			}
+			//is it a gzip (adz) file?
+			else if (data.Length > 3 && data[0] == 0x1F && data[1] == 0x8B)
+			{
+				try
+				{
+					using (var m = new MemoryStream(data))
+					{
+						using (var zip = new GZipStream(m, CompressionMode.Decompress))
+						{
+							using (var ms = new MemoryStream())
+							{
+								zip.CopyTo(ms);
+								data = ms.ToArray();
 							}
 						}
 					}
