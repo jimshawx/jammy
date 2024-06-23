69435644ba455a396e3e87441b9b0820fbe77ff3
Jim <jimshaw@iinet.net.au>
2024-06-23 18:38:57 +1000

    use .net 8 formatting for binary numbers now.

--- Jammy.Extensions/Extensions/Formatting.cs
+++ Jammy.Extensions/Extensions/Formatting.cs
@@ -8,9 +8,14 @@ namespace Jammy.Extensions.Extensions
 {
 	public static class Formatting
 	{
-		public static string ToBin(this byte v) { return ((uint)v).ToBin(8); }
-		public static string ToBin(this ushort v) { return ((uint)v).ToBin(16); }
-		public static string ToBin(this uint v) { return v.ToBin(32); }
-		public static string ToBin(this uint v, int cnt) { return Convert.ToString(v, 2).PadLeft(cnt, '0'); }
+		//public static string ToBin(this byte v) { return ((uint)v).ToBin(8); }
+		//public static string ToBin(this ushort v) { return ((uint)v).ToBin(16); }
+		//public static string ToBin(this uint v) { return v.ToBin(32); }
+		//public static string ToBin(this uint v, int cnt) { return Convert.ToString(v, 2).PadLeft(cnt, '0'); }
+
+		public static string ToBin(this byte v) { return v.ToString("b8"); }
+		public static string ToBin(this ushort v) { return v.ToString("b16"); }
+		public static string ToBin(this uint v) { return v.ToString("b32"); }
+		public static string ToBin(this uint v, int cnt) { return v.ToString($"b{cnt}"); }
 	}
 }
