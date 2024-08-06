f60473e1b7dc5321caccb98b77cc8e45a0ae02bf
Jim <jimshaw@iinet.net.au>
2024-08-06 22:35:07 +0900

    MemoryRange used for emulation, AddressRange used for the UI and debuggers.

--- Jammy.Core.Types/Types/MemoryRange.cs
+++ Jammy.Core.Types/Types/MemoryRange.cs
@@ -1,30 +1,121 @@
 ﻿/*
-	Copyright 2020-2021 James Shaw. All Rights Reserved.
+	Copyright 2020-2024 James Shaw. All Rights Reserved.
 */
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Jammy.Core.Types.Types
 {
-	public class MemoryRange : AddressRange
+	//simple MemoryRange class used by the emulation
+	public class MemoryRange
 	{
-		public MemoryRange() : base(){ }
+		public MemoryRange(uint start, ulong length)
+		{
+			Start = start;
+			Length = length;
+		}
 
-		public MemoryRange(uint start, ulong length) : base(start, length) { }
+		public uint Start { get; set; }
+		public ulong Length { get; set; }
+
+		private ulong End => Start + Length;
+
+		public bool Contains(uint location)
+		{
+			return location >= Start && location < End;
+		}
+	}
+
+	//more complex classes used by the front end and debuggers
+	public class AddressRange
+	{
+		/// <summary>
+		/// inclusive start position
+		/// </summary>
+		public uint Start { get; set; }
+
+		/// <summary>
+		/// exclusive (one past the end) end position
+		/// </summary>
+		public ulong End { get { return Start + Length; } set { Length = value - Start; } }
+
+		public ulong Length { get; set; }
+
+		public AddressRange() { }
+
+		public AddressRange(uint start, ulong length)
+		{
+			this.Start = start;
+			this.Length = length;
+		}
+
+		public bool Overlaps(AddressRange other)
+		{
+			return !(this.Start >= other.End || this.End <= other.Start);
+		}
+
+		public bool Contains(AddressRange other)
+		{
+			return other.Start >= this.Start && other.End <= this.End;
+		}
+
+		public bool Contains(uint location)
+		{
+			return location >= Start && location <= End;
+		}
+
+		public static List<AddressRange> NoOverlaps(List<AddressRange> ranges)
+		{
+			var merge = new List<AddressRange>();
+
+			foreach (var incoming in ranges)
+			{
+				//remove any existing ranges completely contained in the incoming
+				merge.RemoveAll(x => incoming.Contains(x));
+
+				//incoming doesn't overlap anything, add it, and we're done
+				if (!merge.Any(x => x.Overlaps(incoming))) { merge.Add(incoming); continue; }
+
+				//incoming is contained entirely within another, ignore it
+				if (merge.Any(x => x.Contains(incoming))) continue;
+
+				//it partly overlaps one or more existing ranges
+				foreach (var merged in merge)
+				{
+					if (incoming.Overlaps(merged))
+					{
+						//that means incoming contains the start or the end of merged
+
+						//it contains the start, so extend it to the end
+						if (merged.Contains(incoming.Start))
+							merged.End = incoming.End;
+						else
+							merged.Start = incoming.Start;
+					}
+				}
+			}
+			return merge;
+		}
 	}
 
 	public class BulkMemoryRange : AddressRange
 	{
 		public byte[] Memory { get; set; } = [];
 
-		public new ulong End {
+		public new ulong End
+		{
 			get => Start + Length;
 			set => throw new NotSupportedException();
 		}
 
-		public new ulong Length {
+		public new ulong Length
+		{
 			get => (ulong)Memory.Length;
 			set => throw new NotSupportedException();
 		}
 	}
+
 }
+
--- Jammy.Core.Types/Types/Types.cs
+++ Jammy.Core.Types/Types/Types.cs
@@ -1,9 +1,6 @@
 ﻿/*
-	Copyright 2020-2021 James Shaw. All Rights Reserved.
+	Copyright 2020-2024 James Shaw. All Rights Reserved.
 */
-
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Jammy.Core.Types.Types
 {
@@ -20,76 +17,5 @@ namespace Jammy.Core.Types.Types
 	{
 		public int X { get; set; }
 		public int Y { get; set; }
-	}
-
-	public class AddressRange
-	{
-		/// <summary>
-		/// inclusive start position
-		/// </summary>
-		public uint Start { get; set; }
-
-		/// <summary>
-		/// exclusive (one past the end) end position
-		/// </summary>
-		public ulong End { get { return Start + Length; } set { Length = value - Start; } }
-
-		public ulong Length { get; set; }
-
-		public AddressRange(){}
-
-		public AddressRange(uint start, ulong length)
-		{
-			this.Start = start;
-			this.Length = length;
-		}
-
-		public bool Overlaps(AddressRange other)
-		{
-			return !(this.Start >= other.End || this.End <= other.Start);
-		}
-
-		public bool Contains(AddressRange other)
-		{
-			return other.Start >= this.Start && other.End <= this.End;
-		}
-
-		public bool Contains(uint location)
-		{
-			return location >= Start && location <= End;
-		}
-
-		public static List<AddressRange> NoOverlaps(List<AddressRange> ranges)
-		{
-			var merge = new List<AddressRange>();
-
-			foreach (var incoming in ranges)
-			{
-				//remove any existing ranges completely contained in the incoming
-				merge.RemoveAll(x => incoming.Contains(x));
-
-				//incoming doesn't overlap anything, add it, and we're done
-				if (!merge.Any(x => x.Overlaps(incoming))) { merge.Add(incoming); continue; }
-
-				//incoming is contained entirely within another, ignore it
-				if (merge.Any(x => x.Contains(incoming))) continue;
-
-				//it partly overlaps one or more existing ranges
-				foreach (var merged in merge)
-				{
-					if (incoming.Overlaps(merged))
-					{
-						//that means incoming contains the start or the end of merged
-
-						//it contains the start, so extend it to the end
-						if (merged.Contains(incoming.Start))
-							merged.End = incoming.End;
-						else
-							merged.Start = incoming.Start;
-					}
-				}
-			}
-			return merge;
-		}
 	}
 }
