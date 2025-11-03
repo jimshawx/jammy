d1d79cea5b3b1e9b6291102707367501019a7449
Jim <jimshaw@iinet.net.au>
2025-11-03 18:23:12 +0400

    add some stubs for computing ecc for unencoded cdrom data

--- Jammy.Core/CDROM/CDImage.cs
+++ Jammy.Core/CDROM/CDImage.cs
@@ -1,4 +1,6 @@
 ﻿using Jammy.Core.Interface.Interfaces;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -45,12 +47,86 @@ namespace Jammy.Core.CDROM
 			return sector;
 		}
 
+
 		private byte[] EncodeSector(byte[] sector)
 		{
-			//todo - add error correction
 			byte[] header = { 0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0 };
 			byte[] mode = {0,0,0,1 };
-			return header.Concat(mode).Concat(sector).ToArray();
+			byte[] pad = {0,0,0,0,0,0,0,0 };
+			
+			//header + data
+			var data = header.Concat(mode).Concat(sector);
+
+			//add crc32 and an 8 byte pad
+			var cc = data.Concat(CdromCrc32.Compute(data)).Concat(pad).ToArray();
+			
+			//P-Parity
+			var p = P(cc[12..2075]);
+			var ecc = cc.Concat(p).ToArray();
+
+			//Q-Parity
+			var q = Q(ecc[12..2247]);
+			var parity = ecc.Concat(q).ToArray();
+
+			//Scramble?
+			var scrambled = Scramble(parity[12..2351]);
+
+			//final ECC data
+			return scrambled;
+		}
+
+		//todo
+		private byte[] P(ReadOnlySpan<byte> data)
+		{
+			return new byte[172];
+		}
+
+		//todo
+		private byte[] Q(ReadOnlySpan<byte> data)
+		{
+			return new byte[104];
+		}
+
+		//todo
+		private byte[] Scramble(ReadOnlySpan<byte> data)
+		{
+			return data.ToArray();
+		}
+	}
+
+	//P(x) = (x^16 + x^15 + x^2 + 1) . (x^16 + x^2 + x + 1) 
+	public class CdromCrc32
+	{
+		private const uint Polynomial = 0x04C11DB7;
+		private static readonly uint[] Table = new uint[256];
+
+		static CdromCrc32()
+		{
+			for (uint i = 0; i < Table.Length; i++)
+			{
+				uint crc = i << 24;
+				for (int j = 0; j < 8; j++)
+				{
+					if ((crc & 0x80000000) != 0)
+						crc = (crc << 1) ^ Polynomial;
+					else
+						crc <<= 1;
+				}
+				Table[i] = crc;
+			}
+		}
+
+		public static byte[] Compute(IEnumerable<byte> data)
+		{
+			uint crc = 0x00000000;
+
+			foreach (byte b in data)
+			{
+				byte index = (byte)((crc >> 24) ^ b);
+				crc = (crc << 8) ^ Table[index];
+			}
+
+			return BitConverter.GetBytes(crc);
 		}
 	}
 }
