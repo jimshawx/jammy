4e61b1b0ab73d2c7f0313332902440d2141eeb0d
Jim <jimshaw@iinet.net.au>
2025-12-11 19:39:06 +0000

    add some keyboard and mouse emulation to Linux IO

---
+++ Jammy.Core.Interface/Interfaces/IInputOutput.cs
@@ -0,0 +1,28 @@
+﻿/*
+	Copyright 2020-2025 James Shaw. All Rights Reserved.
+*/
+
+using System;
+
+namespace Jammy.Core.Interface.Interfaces
+{
+	public class InputOutput
+	{
+		[Flags]
+		public enum MouseButton
+		{
+			MouseLeft = 1<<0,
+			MouseMiddle = 1<<1,
+			MouseRight = 1<<2
+		}
+
+		public MouseButton MouseButtons { get; set; }
+		public int MouseX { get; set; }
+		public int MouseY { get; set; }
+	}
+
+	public interface IInputOutput
+	{
+		public InputOutput GetInputOutput();
+	}
+}
