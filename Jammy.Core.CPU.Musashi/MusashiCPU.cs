bfa1fef6c654162e45e1640f467abdd0eae2fc34
Jim <jimshaw@iinet.net.au>
2026-06-01 23:01:14 +1000

    use some 'unsafe' code to avoid the cost of reverse pinvoke when the cpu calls back into C# for Musashi C++ CPU

--- Jammy.Core.CPU.Musashi/MusashiCPU.cs
+++ Jammy.Core.CPU.Musashi/MusashiCPU.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Linq;
-using System.Runtime.InteropServices;
-using Jammy.Core.Interface.Interfaces;
+﻿using Jammy.Core.Interface.Interfaces;
 using Jammy.Core.Persistence;
 using Jammy.Core.Types;
 using Jammy.Core.Types.Types;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 /*
 	Copyright 2020-2021 James Shaw. All Rights Reserved.
@@ -31,32 +33,32 @@ namespace Jammy.Core.CPU.Musashi
 
 	public class MusashiCPU : MusashiCPUInternal
 	{	
-		public MusashiCPU(IInterrupt interrupt, IMemoryMapper memoryMapper, IBreakpointCollection breakpoints, ITracer tracer, ILogger<MusashiCPU> logger) :
-			base(MusashiCPUType.M68K_CPU_TYPE_68000, interrupt, memoryMapper, breakpoints, tracer, logger)
+		public MusashiCPU(IInterrupt interrupt, IMemoryMapper memoryMapper, IBreakpointCollection breakpoints, ITracer tracer, IOptions<EmulationSettings> settings, ILogger<MusashiCPU> logger) :
+			base(MusashiCPUType.M68K_CPU_TYPE_68000, interrupt, memoryMapper, breakpoints, tracer, settings, logger)
 		{
 		}
 	}
 
 	public class Musashi68EC020CPU : MusashiCPUInternal
 	{
-		public Musashi68EC020CPU(IInterrupt interrupt, IMemoryMapper memoryMapper, IBreakpointCollection breakpoints, ITracer tracer, ILogger<MusashiCPU> logger) :
-			base(MusashiCPUType.M68K_CPU_TYPE_68EC020, interrupt, memoryMapper, breakpoints, tracer, logger)
+		public Musashi68EC020CPU(IInterrupt interrupt, IMemoryMapper memoryMapper, IBreakpointCollection breakpoints, ITracer tracer, IOptions<EmulationSettings> settings, ILogger<MusashiCPU> logger) :
+			base(MusashiCPUType.M68K_CPU_TYPE_68EC020, interrupt, memoryMapper, breakpoints, tracer, settings, logger)
 		{
 		}
 	}
 
 	public class Musashi68030CPU : MusashiCPUInternal
 	{
-		public Musashi68030CPU(IInterrupt interrupt, IMemoryMapper memoryMapper, IBreakpointCollection breakpoints, ITracer tracer, ILogger<MusashiCPU> logger) :
-			base(MusashiCPUType.M68K_CPU_TYPE_68030, interrupt, memoryMapper, breakpoints, tracer, logger)
+		public Musashi68030CPU(IInterrupt interrupt, IMemoryMapper memoryMapper, IBreakpointCollection breakpoints, ITracer tracer, IOptions<EmulationSettings> settings, ILogger<MusashiCPU> logger) :
+			base(MusashiCPUType.M68K_CPU_TYPE_68030, interrupt, memoryMapper, breakpoints, tracer, settings, logger)
 		{
 		}
 	}
 
 	public class Musashi68040CPU : MusashiCPUInternal
 	{
-		public Musashi68040CPU(IInterrupt interrupt, IMemoryMapper memoryMapper, IBreakpointCollection breakpoints, ITracer tracer, ILogger<MusashiCPU> logger) :
-			base(MusashiCPUType.M68K_CPU_TYPE_68040, interrupt, memoryMapper, breakpoints, tracer, logger)
+		public Musashi68040CPU(IInterrupt interrupt, IMemoryMapper memoryMapper, IBreakpointCollection breakpoints, ITracer tracer, IOptions<EmulationSettings> settings, ILogger<MusashiCPU> logger) :
+			base(MusashiCPUType.M68K_CPU_TYPE_68040, interrupt, memoryMapper, breakpoints, tracer, settings, logger)
 		{
 		}
 	}
@@ -67,62 +69,62 @@ namespace Jammy.Core.CPU.Musashi
 		private readonly IMemoryMapper memoryMapper;
 		private readonly IBreakpointCollection breakpoints;
 		private readonly ITracer tracer;
+		private readonly EmulationSettings settings;
 		private readonly MusashiCPUType cpuType;
 
 		[DllImport("Musashi.dll")]
-		static extern void Musashi_init(uint cputype, IntPtr r32, IntPtr r16, IntPtr r8, IntPtr w32, IntPtr w16, IntPtr w8);
+		private static extern unsafe void Musashi_init(
+			IntPtr ctx,
+			uint cputype,
+			delegate* unmanaged[Cdecl]<IntPtr, uint, uint> r32,
+			delegate* unmanaged[Cdecl]<IntPtr, uint, uint> r16,
+			delegate* unmanaged[Cdecl]<IntPtr, uint, uint> r8,
+			delegate* unmanaged[Cdecl]<IntPtr, uint, uint, void> w32,
+			delegate* unmanaged[Cdecl]<IntPtr, uint, uint, void> w16,
+			delegate* unmanaged[Cdecl]<IntPtr, uint, uint, void> w8
+		);
 
 		[DllImport("Musashi.dll")]
-		static extern uint Musashi_execute(ref int cycles);
+		private static extern uint Musashi_execute(ref int cycles);
 
 		[DllImport("Musashi.dll")]
-		static extern void Musashi_get_regs(Musashi_regs regs);
+		private static extern void Musashi_get_regs(Musashi_regs regs);
 
 		[DllImport("Musashi.dll")]
-		static extern void Musashi_set_regs(Musashi_regs regs);
+		private static extern void Musashi_set_regs(Musashi_regs regs);
 
 		[DllImport("Musashi.dll")]
-		static extern void Musashi_set_pc(uint pc);
+		private static extern void Musashi_set_pc(uint pc);
 
 		[DllImport("Musashi.dll")]
-		static extern void Musashi_set_irq(uint levels);
-
-		private Musashi_Reader r32;
-		private Musashi_Reader r16;
-		private Musashi_Reader r8;
-		private Musashi_Writer w32;
-		private Musashi_Writer w16;
-		private Musashi_Writer w8;
+		private static extern void Musashi_set_irq(uint levels);
 
 		public MusashiCPUInternal(MusashiCPUType cpuType, IInterrupt interrupt, IMemoryMapper memoryMapper,
-			IBreakpointCollection breakpoints, ITracer tracer, ILogger<MusashiCPU> logger)
+			IBreakpointCollection breakpoints, ITracer tracer, IOptions<EmulationSettings> settings, ILogger<MusashiCPU> logger)
 		{
 			this.cpuType = cpuType;
 			this.interrupt = interrupt;
 			this.memoryMapper = memoryMapper;
 			this.breakpoints = breakpoints;
 			this.tracer = tracer;
-
+			this.settings = settings.Value;
 			logger.LogTrace($"Starting Musashi C {cpuType.ToString().Substring(14)} CPU");
-
-			r32 = new Musashi_Reader(Musashi_read32);
-			r16 = new Musashi_Reader(Musashi_read16);
-			r8 = new Musashi_Reader(Musashi_read8);
-			w32 = new Musashi_Writer(Musashi_write32);
-			w16 = new Musashi_Writer(Musashi_write16);
-			w8 = new Musashi_Writer(Musashi_write8);
 		}
 
-		public void Initialise()
+		public unsafe void Initialise()
 		{
+			var handle = GCHandle.Alloc(this, GCHandleType.Normal);
+			IntPtr contextPtr = GCHandle.ToIntPtr(handle);
+
 			Musashi_init(
+				contextPtr,
 				(uint)cpuType,
-				Marshal.GetFunctionPointerForDelegate(r32),
-				Marshal.GetFunctionPointerForDelegate(r16),
-				Marshal.GetFunctionPointerForDelegate(r8),
-				Marshal.GetFunctionPointerForDelegate(w32),
-				Marshal.GetFunctionPointerForDelegate(w16),
-				Marshal.GetFunctionPointerForDelegate(w8)
+				&Musashi_read32_thunk,
+				&Musashi_read16_thunk,
+				&Musashi_read8_thunk,
+				&Musashi_write32_thunk,
+				&Musashi_write16_thunk,
+				&Musashi_write8_thunk
 			);
 		}
 
@@ -291,35 +293,95 @@ namespace Jammy.Core.CPU.Musashi
 			Musashi_set_pc(pc);
 		}
 
-		private delegate uint Musashi_Reader(uint address);
-		private delegate void Musashi_Writer(uint address, uint value);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static MusashiCPU FromIntPtr(IntPtr context)
+		{
+			return Unsafe.As<MusashiCPU>(GCHandle.FromIntPtr(context).Target);
+		}
 
+		[UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+		private static uint Musashi_read32_thunk(IntPtr context, uint address)
+		{
+			return FromIntPtr(context).Musashi_read32(address);
+		}
+
+		[UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+		private static uint Musashi_read16_thunk(IntPtr context, uint address)
+		{
+			return FromIntPtr(context).Musashi_read16(address);
+		}
+
+		[UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+		private static uint Musashi_read8_thunk(IntPtr context, uint address)
+		{
+			return FromIntPtr(context).Musashi_read8(address);
+		}
+
+		[UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+		private static void Musashi_write32_thunk(IntPtr context, uint address, uint value)
+		{
+			FromIntPtr(context).Musashi_write32(address, value);
+		}
+
+		[UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+		private static void Musashi_write16_thunk(IntPtr context, uint address, uint value)
+		{
+			FromIntPtr(context).Musashi_write16(address, value);
+		}
+
+		[UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+		private static void Musashi_write8_thunk(IntPtr context, uint address, uint value)
+		{
+			FromIntPtr(context).Musashi_write8(address, value);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private uint Musashi_read32(uint address)
 		{
 			return memoryMapper.Read(instructionStartPC, address, Size.Long);
 		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private uint Musashi_read16(uint address)
 		{
 			//word read at instruction address is instruction fetch
 			if (address == instructionStartPC)
-				return memoryMapper.Fetch(instructionStartPC, address, Size.Word);
-			return memoryMapper.Read(instructionStartPC, address, Size.Word);
+			{
+				uint m = memoryMapper.Fetch(instructionStartPC, address, Size.Word);
+				return m;
+			}
+			uint n = memoryMapper.Read(instructionStartPC, address, Size.Word);
+			return n;
 		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private uint Musashi_read8(uint address)
 		{
 			return memoryMapper.Read(instructionStartPC, address, Size.Byte);
 		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private void Musashi_write32(uint address, uint value)
 		{
 			memoryMapper.Write(instructionStartPC, address, value, Size.Long);
+			if (settings.Tracer.IsEnabled())
+				tracer.Flush(address);
 		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private void Musashi_write16(uint address, uint value)
 		{
 			memoryMapper.Write(instructionStartPC, address, value, Size.Word);
+			if (settings.Tracer.IsEnabled())
+				tracer.Flush(address);
 		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private void Musashi_write8(uint address, uint value)
 		{
 			memoryMapper.Write(instructionStartPC, address, value, Size.Byte);
+			if (settings.Tracer.IsEnabled())
+				tracer.Flush(address & 0xfffffffe);
 		}
 
 		public void Save(JArray obj)
