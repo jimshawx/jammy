abd7e7fb065d96cd46310d3d5808dad3d7281581
Jim <jimshaw@iinet.net.au>
2026-09-15 20:57:17 +0300

    add some more MFM routines

--- Jammy.Core/Floppy/MFM.cs
+++ Jammy.Core/Floppy/MFM.cs
@@ -127,9 +127,119 @@ namespace Jammy.Core.Floppy
 					.Checksum(64, 1088).AsEnumerable());
 			}
 
-			track = track.Concat(new MFMGap().AsEnumerable());
+			//pad to 12688 bytes
+			//track = track.Concat(new MFMGap().AsEnumerable());
+
+			//pad at the start
+			track = new MFMGap().AsEnumerable().Concat(track);
 
 			return track.ToArray();
+		}
+
+		public static byte[] DecodeTrack(ushort[] mfm)
+		{
+			var trackBytes = new byte[11 * 512];
+			int sectorsFound = 0;
+			int i = 0;
+
+			while (i < mfm.Length - 600 && sectorsFound < 11)
+			{
+				if (mfm[i] == 0x4489 && mfm[i + 1] == 0x4489)
+				{
+					int src = i + 2;
+
+					// sector header (4 mfm words)
+					// bytes: [format, track, sector, sectorsToEnd]
+					var headerInfo = DecodeMfmOddEven(mfm.Skip(src).Take(4).ToArray(), 2);
+					byte sectorNum = headerInfo[2];
+					src += 4;
+
+					// label (16 mfm words)
+					// header checksum (4 mfm words)
+					// data checksum (4 mfm words)
+					src += 16 + 4 + 4;
+
+					// sector data (512 mfm words)
+					if (src + 512 <= mfm.Length && sectorNum < 11)
+					{
+						var data = DecodeMfmOddEven(mfm.Skip(src).Take(512).ToArray(), 256);
+						Buffer.BlockCopy(data, 0, trackBytes, sectorNum * 512, 512);
+						sectorsFound++;
+
+						src += 512;
+					}
+
+					i = src;
+				}
+				else
+				{
+					i++;
+				}
+			}
+
+			return trackBytes;
+		}
+
+		//all odd words followed by all even words
+		public static byte[] DecodeMfmOddEven(ushort[] mfmBlock, int numWords)
+		{
+			var output = new byte[numWords * 2];
+			int oddPtr = 0;
+			int evenPtr = numWords;
+			int byteIndex = 0;
+
+			for (int j = 0; j < numWords; j++)
+			{
+				uint oddBits = (uint)((mfmBlock[oddPtr++] << 1) & 0xAAAA);
+				uint evenBits = (uint)(mfmBlock[evenPtr++] & 0x5555);
+
+				ushort dataWord = (ushort)(oddBits | evenBits);
+
+				output[byteIndex++] = (byte)(dataWord >> 8);
+				output[byteIndex++] = (byte)(dataWord & 0xFF);
+			}
+
+			return output;
+		}
+
+		//interleaved odd/even words
+		public static byte[] DecodeMfmWords(ushort[] mfmBlock, int numWords)
+		{
+			var output = new byte[numWords * 2];
+			int mfmPtr = 0;
+			int byteIndex = 0;
+
+			for (int j = 0; j < numWords; j++)
+			{
+				uint oddBits = (uint)((mfmBlock[mfmPtr++] << 1) & 0xAAAA);
+				uint evenBits = (uint)(mfmBlock[mfmPtr++] & 0x5555);
+
+				ushort dataWord = (ushort)(oddBits | evenBits);
+
+				output[byteIndex++] = (byte)(dataWord >> 8);
+				output[byteIndex++] = (byte)(dataWord & 0xFF);
+			}
+
+			return output;
+		}
+
+		//interleaved odd/even bits
+		public static byte[] DecodeMfmBits(ushort[] mfmBlock, int numWords)
+		{
+			var output = new byte[numWords];
+
+			for (int i = 0; i < numWords; i++)
+			{
+				uint mfm = (uint)(mfmBlock[i] & 0x5555);
+
+				mfm = (mfm | (mfm >> 1)) & 0x3333;
+				mfm = (mfm | (mfm >> 2)) & 0x0F0F;
+				mfm = (mfm | (mfm >> 4)) & 0x00FF;
+
+				output[i] = (byte)mfm;
+			}
+
+			return output;
 		}
 	}
 }
